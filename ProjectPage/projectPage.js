51aaf0b655c9d52e550e8bd5fb36aeb93c77649b
Felipe Rezende <f.zende@gmail.com>
2023-03-16 10:05:59 +0100

    Responsive HTML, CSS,JS done

--- ProjectPage/projectPage.js
+++ ProjectPage/projectPage.js
@@ -2,7 +2,6 @@ const SERVER_URL = "https://raw.githubusercontent.com/ironhack-jc/mid-term-api/m
 
 window.onload = () => {
     
-    //Para que me devuelva la query param que he agragado a la homepage en el enlace de Simplify ?leon=simplify
     function _getProjectTitleOnURL() {
         const params = new Proxy(new URLSearchParams(window.location.search), {
             get: (searchParams, prop) => searchParams.get(prop),
@@ -10,7 +9,6 @@ window.onload = () => {
           console.log(params)
           return params.title.toLowerCase();
     }
-    //Para obtener los datos del servidor
    async function _getProjectData () {
       const response = await  fetch(SERVER_URL)
       const projects = await response.json()
@@ -19,20 +17,10 @@ window.onload = () => {
       console.log(projectToShow)
       _updateProjectData(projectToShow)
     }
-    // Actualiza los datos y los imprime en pantalla
    function _updateProjectData(projectToShow) {
         const projectTitleHeader = document.querySelector(".projectSimplify")
-        /*const projectSubtitle = document.querySelector(“.subtitle”)
-        const projectData = document.querySelector(“.simplifyDateSpam”)
-        const paragraph = document.querySelector(“.paragraphs”)
-        const logoBlur = document.querySelector(“.projectLogoBlur”)
-        const logo = document.querySelector(“.projectLogo”)*/
         projectTitleHeader.textContent = `${projectToShow.name} Felipe`
-        /*projectSubtitle.textContent = projectToShow.description
-        projectData.textContent = projectToShow.completed_on
-        paragraph.textContent = projectToShow.content
-        logoBlur.src = projectToShow.image
-        logo.src = projectToShow.image*/
+    
     };
     _getProjectData();
 };
